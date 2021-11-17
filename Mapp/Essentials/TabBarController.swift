0b8f46f375432d1fc64fc51aede913259b1601cb
dantoso <xeddantasvfd@gmail.com>
2021-11-17 17:09:02 -0300

    comentarios e escondendo a nav bar na tela do mapa

--- Mapp/Essentials/TabBarController.swift
+++ Mapp/Essentials/TabBarController.swift
@@ -19,14 +19,20 @@ class TabBarController: UITabBarController {
     
 	func setupChildren() {
 		
+		//cria view controllers que serão apresentadas pela tab bar controller
 		let mapNavVc = setUpNavigationController(image: UIImage(systemName: "map"), viewController: MapViewController())
 		let pinNavVc = setUpNavigationController(image: UIImage(systemName: "pin.circle"), viewController: PinViewController())
 		let settingsNavVc = setUpNavigationController(image: UIImage(systemName: "gearshape"), viewController: SettingsViewController())
 		
+		//pra nav bar não aparecer na tela do mapa
+		mapNavVc.isNavigationBarHidden = true
+		
+		//adicionando as view controllers apresentadas pela tab bar controller
 		viewControllers = [mapNavVc, pinNavVc, settingsNavVc]
 		
 	}
 	
+	//cria um navigation controller com uma view controller dentro dele
 	func setUpNavigationController(image: UIImage?, viewController: UIViewController) -> UINavigationController {
 
 		let navigationController = UINavigationController(rootViewController: viewController)
