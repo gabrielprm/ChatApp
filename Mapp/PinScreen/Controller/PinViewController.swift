479d033eea5ced85dd1bef7d79168bdd4e44022e
Pedro Esli <pedroesli.vn@gmail.com>
2021-11-17 18:03:10 -0300

    Update: added tableview delete trailing action

--- Mapp/PinScreen/Controller/PinViewController.swift
+++ Mapp/PinScreen/Controller/PinViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class PinViewController: UIViewController {
     
+    var alfinetes = [
+        "Universidade Catolica de Brasilia",
+        "Casa",
+        "Shopping JK",
+        "Aeroporto"
+    ]
 
     private var tableView: UITableView = UITableView(frame: CGRect(), style: .insetGrouped)
     
@@ -30,17 +36,29 @@ class PinViewController: UIViewController {
 }
 
 extension PinViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let delete = UIContextualAction(style: .destructive, title: "") { action, view, complitionHandler in
+            complitionHandler(true)
+        }
+        delete.image = UIImage(systemName: "trash")
+        
+        return UISwipeActionsConfiguration(actions: [delete])
+    }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("pressed cell at: \(indexPath.row)")
+    }
 }
 
 extension PinViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return alfinetes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PinCell", for: indexPath)
-        cell.textLabel?.text = "Hey"
+        cell.accessoryType = .disclosureIndicator
+        cell.textLabel?.text = alfinetes[indexPath.row]
         return cell
     }
 }
