c1cb8281488e53d7033bc80cfb8dc76e1f68b3c0
dantoso <xeddantasvfd@gmail.com>
2021-11-17 21:22:11 -0300

    pins sendo adicionados por toque :D

---
+++ Mapp/MappScreen/Controller/CLManagerDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  CLManagerDelegate.swift
+//  Mapp
+//
+//  Created by Matheus Dantas on 17/11/21.
+//
+
+import UIKit
+import CoreLocation
+
+extension MapViewController: CLLocationManagerDelegate {
+
+	///Permissão mudou de estado
+	func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+		checkAuthorizationStatus()
+	}
+
+	///Roda toda vez que muda a posição
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+				
+	}
+
+	///Roda quando entra numa region
+	func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+		
+	}
+
+	///Roda quando sai de uma region
+	func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+		
+	}
+
+	///Quando começa a monitorar uma region
+	func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {
+		
+	}
+
+}
---
+++ Mapp/MappScreen/Controller/MapViewDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewDelegate.swift
+//  Mapp
+//
+//  Created by Matheus Dantas on 17/11/21.
+//
+
+import MapKit
+
+extension MapViewController: MKMapViewDelegate {
+	
+	///Roda quando a region do mapa mudou por algum motivo
+	func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+		
+	}
+
+}
